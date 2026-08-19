d8eac48df8a3a65db07cf79c4ffe26eb6f846670
rorenstechnologies-wq <rorenstechnologies@gmail.com>
2026-08-19 19:54:19 +0530

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,7 @@
+# ============================================================
+# VERCEL ENTRYPOINT FOR FLASK
+# ============================================================
+
+from app import app
+
+# Vercel expects the Flask application object to be named "app"
